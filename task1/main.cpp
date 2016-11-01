a7b900b6e38095cdc5b86573b4a63138691ca6b9
Reece Volker <reecevolker@gmail.com>
2016-11-01 21:00:40 +0200

    Done

--- task1/main.cpp
+++ task1/main.cpp
@@ -4,16 +4,30 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	string array[]  = {"A","B","C","D"};
-	Node *one = new Node(4,array);
+	int size;
+
+	if( argc < 1 || argc > 2) {
+		cout << "ERROR" << endl;
+		return -1;
+	} else if (argc == 1) {
+		size = 4;
+	} else {
+		size = atoi(argv[1]);
+	}
+
+	string array[size];
+
+	for(int i = 0; i < size; ++i) {
+		array[i] = to_string(i);
+	}
+
+	Node *one = new Node(size,array);
 
 	cout<<"Original List: "<<endl;
-	one->display();
 
 	one->reverse();
 
 	cout<<"Reversed List: "<<endl;
-	one->display();
 
 	return 0;
 }
